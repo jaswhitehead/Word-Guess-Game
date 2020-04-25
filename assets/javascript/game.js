decf53d44cb59b28b569540f54a33845310c9725
Jason Whitehead <jaswhitehead@gmail.com>
2020-04-25 17:39:01 -0500

    game.js logic and variables are set.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,53 @@
+//**First I set up all my variables for outcomes */
+var underscoresLabel = document.querySelector("#underscores");
+var remainingLabel = document.querySelector("#remaining-guesses");
+var attemptsLabel = document.querySelector("#attempts");
 
-//Gets computer to generate a random letter that will serve as the correct answer for the game**/
+//**This offers a wordbank of potential answers */
+var wordBank = ["stewie", "quagmire", "brian", "herbertthepervert", "Lois", "Meg", "peter", "cleveland"];
 
-var emptyStrong
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var guess;
-var guessCount = 0;
-var priorGuess = 0;
-var correctGuess = false; 
-
-function getRandomNumber(upper){
-    var num = Math.floor(Math.random()*upper) + 1;
-    return num;
+//**This sets the total amount of attempts allowed and creates an array to record the numbers */
+var attempts = []
+var remainingAttempts = 10;
+// Main Functionality Starts
+var wordToGuess = getWord();
+updateLabels()
+function getWord() {
+    var wordsSize = wordBank.length;
+    var indexWord = Math.floor(Math.random() * wordsSize);
+    return wordBank[indexWord];
 }
-while(true){
-    guess = prompt('I am thinking of a random letter between A-Z.....What is it?');
-    guessCount += 1;
-    if (parseInt(guess) === randomNumber){
-        correctGuess = true;
-        break;
+function updateLabels() {
+    underscoresLabel.textContent = updateBlanks()
+    remainingLabel.textContent = remainingAttempts;
+    attemptsLabel.textContent = attempts.toString();
+}
+function updateBlanks() {
+    var blanks = []
+    // Go through each letter in wordToGuess
+    for(var letter of wordToGuess) {
+        if(attempts.includes(letter)) {
+            blanks.push(letter)
+        } else {
+            blanks.push('_')
+        }
     }
+    return blanks.join(' ')
 }
-document.write('<h1>You guessed the correct letter!</h1>');
-document.write('it took you' + guessCount + ' tries to guess the number' + randomNumber);
+
+//*Event Listener is to watch for correct keys corresponding to randomly chosen name from wordbank
+document.addEventListener("keypress", function(e) {
+    var keyPressed = e.key.toLocaleLowerCase();
+    if(!attempts.includes(keyPressed)) {
+        attempts.push(keyPressed);
+        if(!wordToGuess.includes(keyPressed)) {
+            remainingAttempts--;
+        }
+    }
+    // Decrease attempts by one when key pressed
+    updateLabels();
+    if(remainingAttempts == 0) {
+        alert("Game Over!")
+        window.location.reload(true);
+    }
+})
